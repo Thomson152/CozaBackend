c9981c43f7563ecdc23c2d7b51ca56ed058837ee
Thomson152 <tomsinonyedikachi@gmail.com>
2023-03-11 11:04:15 +0100

    fixed login issuse

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 /** @format */
 
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import dotenv from "dotenv";
 import MongoDB from "./Config/db.js";
-import userRoute from './routes/userRoute.js'
+import userRoute from "./routes/userRoute.js";
 // import orderRoute from './routes/orderRoute.js'
 import productsRoute from "./routes/productsRoute.js";
 import { errorHandler, notFound } from "./middleware/errorMiddlewares.js";
 
-
 dotenv.config();
 const app = express();
 MongoDB();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-
 
 app.use("/api/products", productsRoute);
 //  app.use('/api/orders', orderRoute)
-app.use('/api/users', userRoute)
+app.use("/api/users", userRoute);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Runing on localhost ${PORT}`));
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,24 +9,23 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body; //receiving email and password from the frontend
-  const user = User.findOne({ email }); // finding the user
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    //checking if the user exit and user password matches the password in the database
     res.json({
       _id: user._id,
-      email: user.email,
-      password: user.password,
       name: user.name,
+      email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
 
 // @desc    Register a new user
