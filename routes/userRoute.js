72f6ebb0b3f9076e757c015953391a65b6ccffad
Thomson152 <tomsinonyedikachi@gmail.com>
2023-04-29 17:01:21 +0100

    bug fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,21 @@
+/** @format */
 
-import express from 'express'
-import { authUser, getUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userControllers.js'
-const router = express.Router()
+import express from "express";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userControllers.js";
+const router = express.Router();
 
-import {protect} from '../middleware/authMiddlewares.js'
+import { protect } from "../middleware/authMiddlewares.js";
 
-router.route("/").post(registerUser)
-router.route('/login').post(authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/").post(registerUser);
+router.route("/login").post(authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
-
-export default router
+export default router;
